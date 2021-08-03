d55bbd4751079b634d136d26850717a61c17e973
Vinicius Barros <vbarros.2001@hotmail.com>
2021-08-03 20:26:49 -0300

    Criando Classe Domain

--- src/main/java/com/vinicius/resorces/CategoriaResorces.java
+++ src/main/java/com/vinicius/resorces/CategoriaResorces.java
@@ -1,16 +1,28 @@
 package com.vinicius.resorces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.vinicius.dodmain.Categoria;
+
 @RestController
-@RequestMapping(value = "/Categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResorces {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 
