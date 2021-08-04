9bd20472134ceb355d09141f62a8ca45106fa3d6
Vinicius Barros <vbarros.2001@hotmail.com>
2021-08-03 21:21:41 -0300

    Banco de dados H2 e a criação da base de dados

--- src/main/java/com/vinicius/dodmain/Categoria.java
+++ src/main/java/com/vinicius/dodmain/Categoria.java
@@ -2,10 +2,19 @@ package com.vinicius.dodmain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	// o serializable serve pra falar que todos os 
 	//dados dessa classe podem ser transformadas em uma sequencia numerica
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	
