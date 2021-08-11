dfef16c5bf297445bbac27f40a18196ffb0bcae9
Vinicius Barros <vbarros.2001@hotmail.com>
2021-08-10 21:38:02 -0300

    Criando operação de instanciação

--- src/main/java/com/vinicius/dodmain/Categoria.java
+++ src/main/java/com/vinicius/dodmain/Categoria.java
@@ -22,17 +22,17 @@ public class Categoria  implements Serializable{
 		
 	}
 
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
